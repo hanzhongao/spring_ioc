9bd3e45fbca62dbe72687d478af40736364eacd3
hza <hanzhongao@outlook.com>
2019-09-04 22:58:31 +0800

    Bean的作用范围测试

---
+++ src/main/java/com/imooc/ioc/demo3/Persion.java
@@ -0,0 +1,8 @@
+package com.imooc.ioc.demo3;
+
+/**
+ * Create by hza
+ * 2019-09-04 22:42
+ */
+public class Persion {
+}
---
+++ src/main/java/com/imooc/ioc/demo3/SpringBeanTest3.java
@@ -0,0 +1,22 @@
+package com.imooc.ioc.demo3;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Bean的作用范围测试
+ */
+public class SpringBeanTest3 {
+    @Test
+    public void test1() {
+        // 创建工厂
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml") ;
+        // 通过工厂创建类
+        Persion persion1 = (Persion) applicationContext.getBean("persion");
+        Persion persion2 = (Persion) applicationContext.getBean("persion");
+
+        System.out.println(persion1);
+        System.out.println(persion2);
+    }
+}
